8f77d18e516ca40a6a0b107d9f7c8b676cd30ce6
Willian-code <willianrosendodelima@outlook.com>
2022-10-01 14:54:31 -0300

    adding the indentation

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -60,13 +60,13 @@ public class TicTacToe {
     }
 
     static void printBoard() {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " | ");
-        System.out.println("|---+---+---|");
-        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " | ");
-        System.out.println("|---+---+---|");
-        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " | ");
-        System.out.println("|---|---|---|");
+        System.out.println("\t\t|---|---|---|");
+        System.out.println("\t\t| " + board[0] + " | " + board[1] + " | " + board[2] + " | ");
+        System.out.println("\t\t|---+---+---|");
+        System.out.println("\t\t| " + board[3] + " | " + board[4] + " | " + board[5] + " | ");
+        System.out.println("\t\t|---+---+---|");
+        System.out.println("\t\t| " + board[6] + " | " + board[7] + " | " + board[8] + " | ");
+        System.out.println("\t\t|---|---|---|");
     }
 
     public static void main(String[] args) {
