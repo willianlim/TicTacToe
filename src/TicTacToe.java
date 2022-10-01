d1d49e5c9d4f77bfcdabc608dda6e1dc64e6eb1d
Willian-code <willianrosendodelima@outlook.com>
2022-09-30 22:01:48 -0300

    build a game board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+
     public static void main(String[] args) {
-        System.out.println();
+        char [][] gameBoard = {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+        printGameBoard(gameBoard);
+    }
+
+    public static void printGameBoard(char [][] gameBoard) {
+        for (char[] row : gameBoard) {
+            System.out.println(row);
+        }
     }
 }
